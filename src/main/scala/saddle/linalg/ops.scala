00bb9a6d9142f2ed040bb4386c4508cdce89d85f
Istvan Bartha <bartha.pityu@gmail.com>
2016-10-07 23:09:42 -0700

    comment

--- src/main/scala/saddle/linalg/ops.scala
+++ src/main/scala/saddle/linalg/ops.scala
@@ -22,6 +22,4 @@ trait GeneralSVD
 
 // from lapack:
 // inverse of A'A from A (QR) (http://scicomp.stackexchange.com/questions/3188/dealing-with-the-inverse-of-a-positive-definite-symmetric-covariance-matrix)
-// svd https://en.wikipedia.org/wiki/Singular_value_decomposition#Calculating_the_SVD
-// http://www.netlib.org/lapack/lapack-3.1.1/html/dgesdd.f.html
 // eigen (symmetric + nonsymmetric)
